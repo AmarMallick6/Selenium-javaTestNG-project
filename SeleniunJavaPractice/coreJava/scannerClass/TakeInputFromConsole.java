b07e180a7eaad58cddf22edbc862ec81a772c9d3
MAK <MAK@143MAK>
2024-08-05 08:19:40 +0530

    Commit: Use of Scanner Class

---
+++ SeleniunJavaPractice/coreJava/scannerClass/TakeInputFromConsole.java
@@ -0,0 +1,21 @@
+package scannerClass;
+
+import java.util.Scanner;
+
+public class TakeInputFromConsole {
+
+	public static void main(String[] args) {
+		//If the input value is undefined only next() method we can use this case ,it take all data as a String Format.............
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter value :");
+		 int value= sc.nextInt();              
+		 System.out.println(" Value is :" + value);
+		 System.out.println("Enter  Decimal value :");
+          double x=sc.nextDouble();
+          System.out.println("Decimal Value is :" + x);
+          System.out.println("Enter String value :");
+          String xy=sc.next();
+          System.out.println(" String Value is :" + xy);
+	}
+
+}
