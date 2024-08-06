dca72959d7d7847fce0a39015c2a886983558228
MAK <MAK@143MAK>
2024-08-06 12:11:20 +0530

    Commit: String methods and Mutable/Immutable Feature code

--- SeleniunJavaPractice/src/test/java/javaCode/FindGreaterNumber.java
+++ SeleniunJavaPractice/src/test/java/javaCode/FindGreaterNumber.java
@@ -7,6 +7,8 @@ public class FindGreaterNumber {
 		int a=393,b=433;
 		int x=(a>b)? a:b;     //result=433(b is big)
       System.out.println(x);
+      int m=(a<b)? a:b;     //result=393(b is big)
+      System.out.println(m);
 	}
 
 }
