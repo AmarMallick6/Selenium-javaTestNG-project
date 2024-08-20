1879b7d66d0487893c69baed5d2849d9cc0f3d0f
MAK <MAK@143MAK>
2024-08-20 13:04:00 +0530

    Commit: update Operator code

--- SeleniunJavaPractice/coreJava/operators/JavaOperators.java
+++ SeleniunJavaPractice/coreJava/operators/JavaOperators.java
@@ -11,7 +11,7 @@ public class JavaOperators {
 		System.out.println(a/b);  //5
 		System.out.println(a%b);  //0
 		
-	//Relational/conditional Operators are ::---  ( ==,   != ,  < ,  >,   <= , >= ,?)
+	//Relational/comparisonal Operators are ::---  ( ==,   != ,  < ,  >,   <= , >= )
 		short x=10,y=20,z=10;
 		System.out.println(x==z);   //true
 		System.out.println(x!=z);    //false
@@ -21,11 +21,57 @@ public class JavaOperators {
 		System.out.println(x>=y);       //false
 		
 	// Assignment  Operators are ::---  ( =,  + = ,  -= ,  *=,   /= , %= )
-		
+		int c= 5;
+		System.out.println(c=c+2); //or we can write  c+=2 , Answer=7
+		System.out.println(c=c-2);  //or we can write  c-=2 , Answer=5
+		System.out.println(c=c*2);  //or we can write  c*=2 , Answer=10
+		System.out.println(c=c/2);  //or we can write  c/=2 , Answer=5
+		System.out.println(c=c%2); //or we can write  c%=2 , Answer=1
 		
 	// Logical  Operators are ::---  (&& ,  || , ! )
+		boolean m=true,n=false;
+	//It is used two compare between Boolean type of value......................
+		System.out.println(m&&n); //False
+		System.out.println(m||n); //True
+		System.out.println(!m); //False
+		System.out.println(!n); //True
+		System.out.println((2>1)&&(1<2)); //True
+		
 		
 	// Increment & Decrement  Operators are ::---  (++,  -- )
+		
+	/*Further it divided into two groups such as - post Increment/Decrement and pre  Increment/Decrement
+	 In post  Increment/Decrement it will 1st assign the value then start 	Increment/Decrement but, 
+	 In pre Increment/Decrement it will 1st Increment/Decrement value then assigned . 
+	*/	
+		//case-01
+		int s=10;
+		s++;  //s=s+1;
+		System.out.println(s); //11
+	
+		int q=1;
+	/*	int d=q++;   //post Increment
+	  System.out.println(d); //1
+	  System.out.println(q); //2
+	   */
+		
+	/*	int d =++q;   //pre increment
+	 System.out.println(d); //2
+	  System.out.println(q); //2
+	  */
+		
+	/*
+	 * int d=q--;//post decrement 
+	 * System.out.println(d); //1 
+	 * System.out.println(q); //0
+	  */		  
+		int d =--q;   //pre increment
+	     System.out.println(d); //0
+		  System.out.println(q); //0
+		  
+		
+		
+	  
 	}
 
 }
--- SeleniunJavaPractice/coreJava/operators/TernaryOperator.java
+++ SeleniunJavaPractice/coreJava/operators/TernaryOperator.java
@@ -1,9 +1,15 @@
 package operators;
 
+import org.apache.poi.ss.formula.DataValidationEvaluator.OperatorEnum;
+
 public class TernaryOperator {
 
 	public static void main(String[] args) {
-	//syntax >> variable=expressions? condition1:condition2;
+	/*In Ternary Operator 1st it check the Expression if expression is True it will be return Condition 1 as Result .
+                        If Expression is False it will be return Condition 2 as Result.
+   syntax >> variable=expressions? condition1:condition2;
+   */
+		
 		//Example-01>>>>>>>
 		int boy_age=28;
        String  x = (boy_age>=18)?"Adult":"child";
@@ -13,7 +19,7 @@ public class TernaryOperator {
       int a=100,b=20;
        int  c =(a>b)? a:b;
      System.out.println(c);  
-     int  cm =(false)?10:20; 
+     int  cm =(false)?100:20; 
      System.out.println(cm);  
 	}
 
